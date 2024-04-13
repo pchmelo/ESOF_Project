f0ed00db35018bdce7de4edb939d97a12b5ee999
Vasco Melo <vascorochamelo@gmail.com>
2024-04-13 15:50:51 +0100

    asseats

---
+++ esof_project/lib/app/views/extra_pages/product.view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProducDetailsPage extends StatelessWidget {
+  const ProducDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
