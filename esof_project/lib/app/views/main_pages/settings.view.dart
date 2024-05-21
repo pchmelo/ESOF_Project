5be1f5c3f0381942c0d9319f90c9a48cfaceca1f
GabrielCarvalhoLEIC <up202208939@up.pt>
2024-05-21 19:32:19 +0100

    created gherkin tests and added expiration date filters

--- esof_project/lib/app/views/main_pages/settings.view.dart
+++ esof_project/lib/app/views/main_pages/settings.view.dart
@@ -76,7 +76,7 @@ class _SettingsViewState extends State<SettingsView> {
                             fontSize: 18, fontWeight: FontWeight.bold),
                       ),
                       Text(
-                        'Change Settings',
+                        'Change Password',
                         style: TextStyle(fontSize: 16, color: Colors.grey),
                       ),
                     ],
@@ -227,20 +227,6 @@ class _ChangeEmailPasswordPageState extends State<ChangeEmailPasswordPage> {
 
   @override
   Widget build(BuildContext context) {
-    String? validateEmail(String? value) {
-      const pattern = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
-          r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
-          r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
-          r'[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4]'
-          r'[0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9]'
-          r'[0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\'
-          r'x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
-      final regex = RegExp(pattern);
-
-      return value!.isNotEmpty && !regex.hasMatch(value)
-          ? 'Enter a valid email address'
-          : null;
-    }
 
     return loading
         ? Loading()
@@ -257,7 +243,7 @@ class _ChangeEmailPasswordPageState extends State<ChangeEmailPasswordPage> {
                   children: [
                     TextFormField(
                       decoration: TextInputDecoration.copyWith(
-                        hintText: 'Actual Password',
+                        hintText: 'Current Password',
                         errorStyle: TextStyle(
                           color: Colors.black,
                           fontWeight: FontWeight.bold,
--- esof_project/test/test_driver/features/user.features/settings_user.feature
+++ esof_project/test/test_driver/features/user.features/settings_user.feature
@@ -2,7 +2,11 @@ Feature: Change user info
   Users should be able to change their account's information by accessing the settings tab and pressing
   the change settings button.
 
-  Scenario:
-    Given
-    When
-    Then
+  Scenario: Change Password
+    Given I'm in the settings tab
+    When I press the "change Password" button
+    And I enter my current password
+    And I enter my new password
+    And I confirm my new password
+    And I press the "Save" button
+    Then I change my password
