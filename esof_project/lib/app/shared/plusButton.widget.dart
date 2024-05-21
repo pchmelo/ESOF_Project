969313c4d58a613dacdcc06b72b4e9a418ce4644
rafaelcunha02 <rafaelscpcunha@gmail.com>
2024-05-21 18:58:32 +0100

    plusButton widget adjusted to screensize

--- esof_project/lib/app/shared/plusButton.widget.dart
+++ esof_project/lib/app/shared/plusButton.widget.dart
@@ -38,120 +38,123 @@ class _PlusButtonState extends State<PlusButton> {
     return regex.hasMatch(barcode) && barcode.length == 13;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 275,
-      padding: const EdgeInsets.all(15.0),
-      color: Colors.transparent,
-      child: Center(
-        child: Column(
-          children: <Widget>[
-            const Padding(
-              padding: EdgeInsets.fromLTRB(0, 10.0, 0, 0),
-              child: Text(
-                'Select Your Option',
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
+@override
+Widget build(BuildContext context) {
+  double screenHeight = MediaQuery.of(context).size.height;
+  double screenWidth = MediaQuery.of(context).size.width;
+
+  return Container(
+    width: screenWidth,
+    height: screenHeight * 0.35,
+    padding: EdgeInsets.all(screenWidth * 0.02),
+    color: Colors.transparent,
+    child: Center(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, screenHeight * 0.02, 0, 0),
+            child: Text(
+              'Select Your Option',
+              style: TextStyle(
+                fontSize: screenHeight * 0.04,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
               ),
             ),
-            Form(
-              key: _formKey,
-              child: Center(
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.05,
-                    ),
-                    Center(
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          IconButton(
-                            style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.amber),
-                              foregroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.black),
-                              textStyle: MaterialStateProperty.all<TextStyle>(
-                                const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18.0,
-                                ),
-                              ),
-                              minimumSize: MaterialStateProperty.all<Size>(
-                                const Size(110.0, 110.0),
-                              ),
-                              shape: MaterialStateProperty.all<OutlinedBorder>(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(
-                                      20.0), // Decreasing border radius
-                                ),
+          ),
+          Form(
+            key: _formKey,
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: screenHeight * 0.05,
+                  ),
+                  Center(
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(screenWidth * 0.02, 0, screenWidth * 0.02, 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        IconButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.amber),
+                            foregroundColor: MaterialStateProperty.all<Color>(
+                                Colors.black),
+                            textStyle: MaterialStateProperty.all<TextStyle>(
+                              TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: screenHeight * 0.022,
                               ),
                             ),
-                            icon: const Icon(Icons.add),
-                            iconSize: 80,
-                            onPressed: () {
-                              String? currentRoute =
-                                  ModalRoute.of(context)!.settings.name;
-                              if (currentRoute != '/start/add_product') {
-                                Navigator.pushReplacementNamed(context,
-                                    '/start/add_product/manual_add_product');
-                              }
-                            },
-                          ),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.12,
-                          ),
-                          IconButton(
-                            style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.amber),
-                              foregroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.black),
-                              textStyle: MaterialStateProperty.all<TextStyle>(
-                                const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18.0,
-                                ),
-                              ),
-                              minimumSize: MaterialStateProperty.all<Size>(
-                                const Size(110.0, 110.0),
-                              ),
-                              shape: MaterialStateProperty.all<OutlinedBorder>(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(
-                                      20.0), // Decreasing border radius
-                                ),
+                            minimumSize: MaterialStateProperty.all<Size>(
+                              Size(screenWidth * 0.2, screenWidth * 0.2),
+                            ),
+                            shape: MaterialStateProperty.all<OutlinedBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    20.0),
                               ),
                             ),
-                            icon: const Icon(CupertinoIcons.barcode_viewfinder),
-                            iconSize: 80,
-                            onPressed: () {
-                              String? currentRoute =
-                                  ModalRoute.of(context)!.settings.name;
-                              if (currentRoute != '/start/add_product') {
-                                return readCodeBar();
-                              }
-                            },
                           ),
-                        ],
-                      ),
+                          icon: const Icon(Icons.add),
+                          iconSize: screenHeight * 0.15,
+                          onPressed: () {
+                            String? currentRoute =
+                                ModalRoute.of(context)!.settings.name;
+                            if (currentRoute != '/start/add_product') {
+                              Navigator.pushReplacementNamed(context,
+                                  '/start/add_product/manual_add_product');
+                            }
+                          },
+                        ),
+                        SizedBox(
+                          width: screenWidth * 0.125,
+                        ),
+                        IconButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.amber),
+                            foregroundColor: MaterialStateProperty.all<Color>(
+                                Colors.black),
+                            textStyle: MaterialStateProperty.all<TextStyle>(
+                              TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: screenHeight * 0.022,
+                              ),
+                            ),
+                            minimumSize: MaterialStateProperty.all<Size>(
+                              Size(screenWidth * 0.15, screenWidth * 0.15),
+                            ),
+                            shape: MaterialStateProperty.all<OutlinedBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    20.0),
+                              ),
+                            ),
+                          ),
+                          icon: const Icon(CupertinoIcons.barcode_viewfinder),
+                          iconSize: screenHeight * 0.15,
+                          onPressed: () {
+                            String? currentRoute =
+                                ModalRoute.of(context)!.settings.name;
+                            if (currentRoute != '/start/add_product') {
+                              return readCodeBar();
+                            }
+                          },
+                        ),
+                      ],
                     ),
-                    ),
-                  ],
-                ),
+                  ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-    );
-  }
+    ),
+  );
+}
 }
